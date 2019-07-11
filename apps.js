9a3110cd86b712b734dc9c1f70cdc6598fdb66d3
Suhaili Faizzi <suhailifaizzi@Suhailis-MacBook-Pro.local>
2019-07-11 10:37:31 +0800

    Added 2 sliders

--- apps.js
+++ apps.js
@@ -9,49 +9,65 @@ $(document).ready( function () {
     // VARIABLES
     // ----------------------------------------------------------
 
-    var amount, percent, result;
     var calculator = $('.calculator');
-    // var calculatorBill = calculator.find('.calculator__bill');
     var slider1 = calculator.find('.input-1-slider');
-    var calculatorResult = calculator.find('.calculator__result');
+    var slider2 = calculator.find('.input-2-slider');
     var slider1amount = calculator.find('.input-1-amount');
+    var slider2amount = calculator.find('.input-2-amount');
+    var quotationInput1 = calculator.find('.input-1-quote');
+    var quotationInput2 = calculator.find('.input-2-quote');
+    var totalQuote = calculator.find('.total-quote');
 
+    // INITIALIZE PAGE - POPULATE DATA RESPECTIVELY
+    // ----------------------------------------------------------
     $(document).ready(function(){
+        // Initialize to display value of Input 1
         slider1amount.text(slider1.val());
+        // Initialize to display value of Input 1
+        slider2amount.text(slider2.val());
+
+        // Input 1 quotation - 5 is fixed price 5 ringgit
+        quotationInput1.text(5 * slider1.val());
+
+        // Input 2 quotation - 7.50 is fixed price 7.50 ringgit
+        quotationInput2.text(7.5 * slider2.val());
+        
+        // Caluclate value of total quote
+        totalQuotation();
     });
 
+    // SLIDER CHANGE
+    // ----------------------------------------------------------
     slider1.on('input', function(){
+        // Initialize to display value of Input 1
         slider1amount.text(slider1.val());
+
+        // Input 1 quotation - 5 is fixed price 5 ringgit
+        quotationInput1.text(5 * slider1.val());
+
+        // Calculate value of total quote
+        totalQuotation();
     });
 
-/*
-    // INIT BILL
+    slider2.on('input', function(){
+        // Initialize to display value of Input 1
+        slider2amount.text(slider2.val());
+
+        // Input 1 quotation - 5 is fixed price 5 ringgit
+        quotationInput2.text(7.5 * slider2.val());
+
+        // Calculate value of total quote
+        totalQuotation();
+    })
+
+    // CALCULATE TOTAL QUOTATION
     // ----------------------------------------------------------
+    function totalQuotation(){
+        let calculatedQuote = 
+            (slider1.val() * 5) + 
+            (slider2.val() * 7.5);
+        
+        totalQuote.text(calculatedQuote);
+    }
 
-    $(window).on('DOMContentLoaded', function () {
-        tipAmount.text( calculatorTip.val() + '%' );
-        amount = calculatorBill.val() * 1;
-        percent = calculatorTip.val() * 1;
-        result = amount + amount * ( percent / 100 );
-        calculatorResult.text( result.toFixed(2) + '$' );
-    });
-
-
-    // RANGE FUNCTION
-    // ----------------------------------------------------------
-
-    calculatorTip.on('change', function () {
-
-        if ( calculatorBill.val() === '' || isNaN( calculatorBill.val() ) ) {
-            alert('Enter bill amount, please!')
-        } else {
-            amount = calculatorBill.val() * 1;
-        }
-
-        tipAmount.text( calculatorTip.val() + '%' );
-        percent = calculatorTip.val() * 1;
-        result = amount + amount * ( percent / 100 );
-        calculatorResult.text( result.toFixed(2) + '$' );
-    });
-*/
-});
+})
